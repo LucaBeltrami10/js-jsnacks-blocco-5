d1d96175cd6e3007d81840b098a67a73e890ee76
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-27 12:20:04 +0100

    creato map con condizione per return

--- snack-2/script.js
+++ snack-2/script.js
@@ -3,27 +3,27 @@ const persone = [
         nome : 'Mario',
         cognome : 'Biondi',
         età : '12'
-    }
+    },
     {
         nome : 'Maria',
         cognome : 'Mori',
         età : '22'
-    }
+    },
     {
         nome : 'Beppe',
         cognome : 'Rossi',
         età : '66'
-    }
+    },
     {
         nome : 'Giuseppa',
         cognome : 'Verdi',
         età : '17'
-    }
+    },
     {
         nome : 'Franchino',
         cognome : 'Bianchi',
         età : '19'
-    }
+    },
     {
         nome : 'Carla',
         cognome : 'De Carlis',
@@ -31,3 +31,11 @@ const persone = [
     }
 ]
 
+const chiGuida = persone.map((individuo) => {
+    if( individuo.età >= 18){
+        return `${individuo.nome} ${individuo.cognome} può guidare`
+    }
+    return `${individuo.nome} ${individuo.cognome} NON può guidare`
+})
+
+console.log(chiGuida)
