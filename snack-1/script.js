3cf45a11aeb10ed087a2fed7e412d856c424f0a7
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-27 11:39:29 +0100

    inserito valori delle chiavi oggetti

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,7 +1,52 @@
 const cars = [
     {
-        brand : '',
-        model : '',
-        fuel : ''
+        brand : 'Toyota',
+        model : 'Corolla',
+        fuel : 'Elettrico'
+    },
+    {
+        brand : 'Toyota',
+        model : 'Corolla Cross',
+        fuel : 'Benzina'
+    },
+    {
+        brand : 'Toyota',
+        model : 'Yaris',
+        fuel : 'GPL'
+    },
+    {
+        brand : 'Toyota',
+        model : 'Prius',
+        fuel : 'Diesel'
+    },
+    {
+        brand : 'Toyota',
+        model : 'Yaris Cross',
+        fuel : 'GPL'
+    },
+    {
+        brand : 'Audi',
+        model : 'A1',
+        fuel : 'Benzina'
+    },
+    {
+        brand : 'Audi',
+        model : 'A3',
+        fuel : 'Diesel'
+    },
+    {
+        brand : 'Audi',
+        model : 'RS3',
+        fuel : 'Benzina'
+    },
+    {
+        brand : 'Fiat',
+        model : '500',
+        fuel : 'Elettrico'
+    },
+    {
+        brand : 'Fiat',
+        model : 'Doblò',
+        fuel : 'Benzina'
     },
 ]
