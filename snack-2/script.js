6d5eca0b39d68a2f74e10e0c635b5e380dc0d604
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-27 12:14:54 +0100

    creato array di persone

---
+++ snack-2/script.js
@@ -0,0 +1,33 @@
+const persone = [
+    {
+        nome : 'Mario',
+        cognome : 'Biondi',
+        età : '12'
+    }
+    {
+        nome : 'Maria',
+        cognome : 'Mori',
+        età : '22'
+    }
+    {
+        nome : 'Beppe',
+        cognome : 'Rossi',
+        età : '66'
+    }
+    {
+        nome : 'Giuseppa',
+        cognome : 'Verdi',
+        età : '17'
+    }
+    {
+        nome : 'Franchino',
+        cognome : 'Bianchi',
+        età : '19'
+    }
+    {
+        nome : 'Carla',
+        cognome : 'De Carlis',
+        età : '18'
+    }
+]
+
