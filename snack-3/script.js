db3f7b2e7929421429b6e918dafd4b6038df1955
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-28 12:19:38 +0100

    snack-3 da rifare. inizio snack-4

--- snack-3/script.js
+++ snack-3/script.js
@@ -9,5 +9,8 @@ const {tab} = broswer
 console.log(tab)
 
 tab.forEach((finestra, index) => {
-    
+    if (finestra == social.includes()){
+        console.log('finestra da eliminare')
+    }
 })
+
