241bce0381d34c92bc1cb0b22e13a72f31c3dec8
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-28 12:46:52 +0100

    appuntate consegne degli ultimi 3 snack

--- snack-4/script.js
+++ snack-4/script.js
@@ -1,3 +1,5 @@
+/* Scrivi una funzione che fonda due array (aventi lo stesso numero di elementi) prendendo alternativamente gli elementi da uno e dall'altro
+es. [a,b,c], [1,2,3] → [a,1,b,2,c,3]. */
 const lettere = ['a','b','c','d'];
 
 const numeri = ['1','2','3','4'];
--- snack-5/script.js
+++ snack-5/script.js
@@ -1,3 +1,7 @@
+/* Creare un array di oggetti di squadre di volley.
+Ogni squadra avrà diverse proprietà: nome, punti fatti e falli subiti.
+Generare numeri random al posto delle nelle proprietà:
+punti fatti e falli subiti */
 const squadre = [
     {
         nome : 'bolognese',
