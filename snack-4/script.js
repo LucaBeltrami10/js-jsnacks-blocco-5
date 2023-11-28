f720e5382b7274372d58a500ce02263f1de68f01
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-28 12:26:03 +0100

    snack-4 finito

--- snack-4/script.js
+++ snack-4/script.js
@@ -3,3 +3,10 @@ const lettere = ['a','b','c','d'];
 const numeri = ['1','2','3','4'];
 
 const fusione = [];
+
+lettere.forEach((item, index) => {
+    fusione.push(item)
+    fusione.push(numeri[index])
+})
+
+console.log(fusione)
